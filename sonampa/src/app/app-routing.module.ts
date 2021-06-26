c7ba1a54870fc5cbfb38dbb9245d3178fb23d13f
jtshetrim <jtshetrim@gmail.com>
2021-06-26 20:37:54 +0600

    logo

--- sonampa/src/app/app-routing.module.ts
+++ sonampa/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MapViewComponent} from './map-view/map-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'map',
+  component: MapViewComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sonampa/src/app/app.component.ts
+++ sonampa/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sonampa';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  isActive(): string {
+    return this.router.url;
+  }
+
 }
