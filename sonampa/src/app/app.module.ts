fc22f69fd08d8845b0c45de33c4bf85071adf5f8
jtshetrim <jtshetrim@gmail.com>
2021-06-26 22:39:50 +0600

    logo

--- sonampa/src/app/app.module.ts
+++ sonampa/src/app/app.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MapViewComponent } from './map-view/map-view.component';
-import { GoogleMapsModule } from '@angular/google-maps';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MapViewComponent} from './map-view/map-view.component';
+import {GoogleMapsModule} from '@angular/google-maps';
+import {AngularFireModule} from '@angular/fire';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDNaiqc4gAyA97ZZ7W7xRNqMSckHi0s5sg',
+  authDomain: 'hackathon-4f45a.firebaseapp.com',
+  projectId: 'hackathon-4f45a',
+  storageBucket: 'hackathon-4f45a.appspot.com',
+  messagingSenderId: '278492018357',
+  appId: '1:278492018357:web:4b9beec97215a647e770fc',
+  measurementId: 'G-1QGRSLNZ3S'
+};
 
 @NgModule({
   declarations: [
@@ -14,9 +25,11 @@ import { GoogleMapsModule } from '@angular/google-maps';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- sonampa/src/app/map-view/map-view.component.ts
+++ sonampa/src/app/map-view/map-view.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {GoogleMap, MapInfoWindow, MapMarker} from '@angular/google-maps';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-map-view',
@@ -20,36 +21,46 @@ export class MapViewComponent implements OnInit {
     maxZoom: 15,
     minZoom: 8,
   };
-  markers: Marker[] = [
-    {
-      position: {
-        lat: 27.5142,
-        lng: 90.4336
-      },
-      label: {
-        text: 'Thimphu',
-        color: 'red'
-      },
-      info: 'A man died',
-      title: 'Hello',
-      options: {
-        animation: google.maps.Animation.BOUNCE
-      }
-    },
-    {
-      position: {
-        lat: 27.6142,
-        lng: 90.3336
-      },
-      label: {
-        text: 'Gasa',
-        color: 'green'
-      },
-      info: 'A man killed a man',
-      title: 'lingling',
-    }
-  ];
+  markers: Marker[] = [];
+  //   {
+  //     position: {
+  //       lat: 27.5142,
+  //       lng: 90.4336
+  //     },
+  //     label: {
+  //       text: 'Thimphu',
+  //       color: 'red'
+  //     },
+  //     info: 'A man died',
+  //     title: 'Hello',
+  //     options: {
+  //       animation: google.maps.Animation.BOUNCE
+  //     }
+  //   },
+  //   {
+  //     position: {
+  //       lat: 27.6142,
+  //       lng: 90.3336
+  //     },
+  //     label: {
+  //       text: 'Gasa',
+  //       color: 'green'
+  //     },
+  //     info: 'A man killed a man',
+  //     title: 'lingling',
+  //   }
+  // ];
   infoContent = '';
+
+  constructor(private firebase: AngularFirestore) {
+    const things = firebase.collection('diseases').valueChanges();
+    things.subscribe((resp: Interface[]) => {
+      const hel = this.serialize(resp);
+      this.markers = hel;
+      console.log(hel);
+      // this.markers = resp
+    });
+  }
 
   ngOnInit(): void {
     navigator.geolocation.getCurrentPosition(() => {
@@ -61,6 +72,26 @@ export class MapViewComponent implements OnInit {
     this.addMarker(this.markers);
   }
 
+  serialize(data: Interface[]): Marker[] {
+    const finalizedArr: Marker[] = [];
+    data.forEach(el => {
+      const obj: Marker = {
+        position: {
+          lat: el.location._lat,
+          lng: el.location._long
+        },
+        label: {
+          text: 'disease',
+          color: 'white'
+        },
+        info: `<img src="${el.image}" alt="image">`,
+        title: 'disease'
+      };
+      finalizedArr.push(obj);
+    });
+    return finalizedArr;
+  }
+  // disease: ${el.disease} uploaded by: ${el.user.name} phone: ${el.user.phone}
   zoomIn(): void {
     if (this.zoom < this.options.maxZoom) {
       this.zoom++;
@@ -107,6 +138,23 @@ export interface Marker {
   info: string;
   options?: {
     animation: google.maps.Animation;
+  };
+}
+
+interface Interface {
+  location: {
+    _lat: number;
+    _long: number;
+  };
+  disease: {
+    crop: string,
+    disease: string
+  };
+  dzongkhag: string;
+  image: string;
+  user: {
+    name: string;
+    phone: string;
   };
 }
 
